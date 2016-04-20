19a041757b2ecc9f01a02973d42a6c9033d32d94
mphipps <student@bluc02mg32jfh51.local>
2016-04-20 09:58:33 -0500

    Included Button With Image

--- Word Search/ViewController.swift
+++ Word Search/ViewController.swift
@@ -4,22 +4,15 @@
 //
 //  Created by kpritchett on 4/8/16.
 //  Copyright © 2016 kpritchett. All rights reserved.
-//phipps test
+//  Phipps test
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var generateNewSearchButton: UIButton!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
